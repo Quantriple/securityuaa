8f4ced3422efe4afece7462b3866137371e1331e
TripleQ <quanqiquan@hongguaninfo.com>
2022-05-05 16:16:07 +0800

    添加验证用户

--- uaa/src/main/java/com/uaa/config/SecurityConfig.java
+++ uaa/src/main/java/com/uaa/config/SecurityConfig.java
@@ -1,35 +1,47 @@
 package com.uaa.config;
 
+import com.uaa.security.userdetails.UserDetailsServiceImpl;
 import lombok.RequiredArgsConstructor;
+import lombok.val;
 import org.apache.http.HttpStatus;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.MessageDigestPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.zalando.problem.spring.web.advice.security.SecurityProblemSupport;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 @EnableWebSecurity(debug = true)
 @RequiredArgsConstructor
 @Import(SecurityProblemSupport.class)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-private final SecurityProblemSupport securityProblemSupport;
+    private final UserDetailsServiceImpl userDetailsService;
+
+    private final SecurityProblemSupport securityProblemSupport;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
         //super.configure(http);
         //   http.
         // http.formLogin(Customizer.withDefaults()).authorizeRequests(req-> req.mvcMatchers("/api/greeting").authenticated());
-        http.exceptionHandling(exp ->exp.authenticationEntryPoint(securityProblemSupport)
+        http.exceptionHandling(exp -> exp.authenticationEntryPoint(securityProblemSupport)
                 .accessDeniedHandler(securityProblemSupport))
                 .authorizeRequests(req -> req.anyRequest().authenticated())
                 .csrf(csrf -> csrf.disable())
@@ -48,7 +60,27 @@ private final SecurityProblemSupport securityProblemSupport;
     public void configure(WebSecurity web) throws Exception {
         // super.configure(web);
         //  web.ignoring().mvcMatchers();
-        web.ignoring().mvcMatchers("/public/**","/api/greeting")
+        web.ignoring().mvcMatchers("/public/**", "/api/greeting")
                 .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        // 默认编码算法的 Id
+        String idForEncode = "bcrypt";
+        Map<String, PasswordEncoder> encoders = new HashMap<>();
+        // 要支持的多种编码器
+/*        val encoders = Map.of(
+                idForEncode, new BCryptPasswordEncoder(),
+                "SHA-1", new MessageDigestPasswordEncoder("SHA-1")
+        );*/
+        encoders.put("idForEncode", new BCryptPasswordEncoder());
+        encoders.put("SHA-1", new MessageDigestPasswordEncoder("SHA-1"));
+        return new DelegatingPasswordEncoder(idForEncode, encoders);
     }
 }
