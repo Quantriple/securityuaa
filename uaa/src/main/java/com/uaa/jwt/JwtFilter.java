95fcbfa6b1ebc099cc276196d2f455974bd6a185
TripleQ <quanqiquan@hongguaninfo.com>
2022-06-02 16:25:25 +0800

    添加了JwtFilter

--- uaa/src/main/java/com/uaa/jwt/JwtFilter.java
+++ uaa/src/main/java/com/uaa/jwt/JwtFilter.java
@@ -1,5 +1,8 @@
 package com.uaa.jwt;
 
+import com.uaa.config.AppProperties;
+import io.jsonwebtoken.Claims;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -11,14 +14,32 @@ import java.io.IOException;
 
 /**
  * @author TripleQ
- * @description  继承OncePerRequestFilter 而不继承Filter的原因是 serlvet版本的不同会略有差异，所以使用spring的话 用这个比较好
+ * @description 继承OncePerRequestFilter 而不继承Filter的原因是 serlvet版本的不同会略有差异，所以使用spring的话 用这个比较好
  * @date 2022/5/28 16:57:36
  * @VERSION 1.0
  **/
 @Component
+@RequiredArgsConstructor
 public class JwtFilter extends OncePerRequestFilter {
+
+    private final AppProperties appProperties;
+
     @Override
     protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
+        if (checkJwtToken(httpServletRequest)) {
+            // TODO 验证token是否正常
+
+        }
+
+        filterChain.doFilter(httpServletRequest, httpServletResponse);
 
     }
+
+    private boolean checkJwtToken(HttpServletRequest request) {
+        String header = request.getHeader(appProperties.getJwt().getHeader());
+        return header != null && header.startsWith(appProperties.getJwt().getPrefix()) ? true : false;
+
+    }
+
+    private Claims
 }
