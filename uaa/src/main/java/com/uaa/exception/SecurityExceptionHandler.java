2af2885b6d84c3d44cd6fcbb4e496457366116bf
TripleQ <quanqiquan@hongguaninfo.com>
2022-06-24 14:44:23 +0800

    添加jwtfilter

--- uaa/src/main/java/com/uaa/exception/SecurityExceptionHandler.java
+++ uaa/src/main/java/com/uaa/exception/SecurityExceptionHandler.java
@@ -7,7 +7,7 @@ import org.zalando.problem.spring.web.advice.security.SecurityAdviceTrait;
  * @author TripleQ
  * @description
  * @date 2022/4/15 15:09:44
- * @VERSION 1.0
+ * @VERSION 1.0  305  326  335   2384
  **/
 @ControllerAdvice
 public class SecurityExceptionHandler implements SecurityAdviceTrait {
